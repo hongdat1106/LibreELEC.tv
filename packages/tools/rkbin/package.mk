b4c7c422d22b8fbe432ea9c3de484602eedb12a8
knaerzche <you@example.com>
2019-05-18 18:24:13 +0200

    Update rkbin to 4e768bc

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-# Version is: Kwiboo/tag:libreelec-ba436b9
-PKG_VERSION="ba436b9d616318a9437895457f6bbef1cc873e2b"
-PKG_SHA256="72c2ef6ec1fe79da7c701056662343b9e1df1cf20e5df10bafc4ec0619ef4578"
+
+PKG_VERSION="4e768bc73bea3332ae8af181e7e6322c06152c7c"
+PKG_SHA256="38fa62c0ae44b1e12a3f94833d7ce32a9ad74a2b839277f3d29c64e0c97ee572"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
