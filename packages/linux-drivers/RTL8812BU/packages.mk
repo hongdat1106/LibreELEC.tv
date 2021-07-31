e0598b02e29e696833edd4fb36e04721bf996ed9
C.Jin <80704502+hongdat1106@users.noreply.github.com>
2021-08-01 01:54:24 +0700

    Add files via upload

---
+++ packages/linux-drivers/RTL8812BU/packages.mk
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="RTL8812BU"
+PKG_VERSION="5.8.7.4"
+PKG_SHA256="6f1d1ee8f130f0aee3271246a93f4e6c3143f9f56e9e3f64aba2c2415c2f26ca"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/morrownr/88x2bu"
+PKG_URL="https://github.com/morrownr/88x2bu/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_LONGDESC="Realtek RTL8812BU Linux 4.x driver"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    cp *.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+}
