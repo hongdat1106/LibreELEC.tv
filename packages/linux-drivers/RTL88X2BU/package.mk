577c78d81056abc1b3a202e2b60e2a8c67e57d6d
hongdat1106 <80704502+hongdat1106@users.noreply.github.com>
2021-08-02 00:23:30 +0700

    Update package.mk

--- packages/linux-drivers/RTL88X2BU/package.mk
+++ packages/linux-drivers/RTL88X2BU/package.mk
@@ -7,24 +7,35 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/morrownr/88x2bu"
 PKG_URL="https://github.com/morrownr/88x2bu/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
-PKG_TOOLCHAIN="manual"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_LONGDESC="Realtek RTL88x2BU Linux driver"
 PKG_IS_KERNEL_PKG="yes"
 
-pre_make_target() {
-  unset LDFLAGS
-}
+#pre_make_target() {
+#  unset LDFLAGS
+#}
+
+#make_target() {
+#  make V=1 \
+#       ARCH=$TARGET_KERNEL_ARCH \
+#       KSRC=$(kernel_path) \
+#       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+#       CONFIG_POWER_SAVING=n
+#}
+
+#makeinstall_target() {
+#  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+#    cp *.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+#}
 
 make_target() {
-  make V=1 \
-       ARCH=$TARGET_KERNEL_ARCH \
-       KSRC=$(kernel_path) \
-       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
-       CONFIG_POWER_SAVING=n
+  kernel_make V=1 \
+       -C $(kernel_path) \
+       M="$PKG_BUILD/src"
 }
 
 makeinstall_target() {
+ cd $PKG_BUILD
   mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
-    cp *.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    cp src/*.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
 }
